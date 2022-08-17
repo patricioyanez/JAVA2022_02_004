7c599c701b0f9975584ee53c3859e3758e41182a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-17 08:44:18 -0400

    ejemplo

---
+++ Ejemplo1_004/src/Pizza.java
@@ -0,0 +1,26 @@
+/*
+
+Creamos la primera clase
+*/
+
+
+public class Pizza {
+// los atributos
+    private String nombre;
+    private String tamano;
+    private String masa;
+// constructor
+
+// getter and setter
+    
+// metodos customer(personalizado)
+    public void preparar()
+    {
+        System.out.println("Se esta preparando la pizza");
+    }
+    public void calentar()
+    {
+        System.out.println("La pizza esta calentandose");
+    }
+    
+}
---
+++ Ejemplo1_004/src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {    
+    // metodo main
+    public static void main(String[] args)
+    {
+        // creación de objeto a partir de la clase pizza
+        Pizza pizza1 = new Pizza();
+        
+        Pizza pizza2;
+        pizza2 = new Pizza();
+        
+        pizza1.preparar();
+        pizza1.calentar();
+       
+    }    
+}
