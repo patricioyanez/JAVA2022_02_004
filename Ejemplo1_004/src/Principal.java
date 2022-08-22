87089acbc0a101a4276546b5d597f25d6c4d3cea
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-22 17:29:00 -0400

    update

--- Ejemplo1_004/src/Principal.java
+++ Ejemplo1_004/src/Principal.java
@@ -11,6 +11,21 @@ public class Principal {
         
         pizza1.preparar();
         pizza1.calentar();
-       
+        pizza2.calentar();
+        
+        Pizza pizza3 = new Pizza("Napolitana","Familiar", "Delgada");
+        
+//        System.out.println("**** Datos de la Pizza");
+//        System.out.println("Nombre  : " + pizza3.getNombre());
+//        pizza3.setNombre("NapoChilena");
+//        System.out.println("Nombre  : " + pizza3.getNombre());
+//        System.out.println("Tamaño  : " + pizza3.getTamano());
+//        System.out.println("Masa    : " + pizza3.getMasa());
+        pizza1.setNombre("Texana");
+        pizza1.setTamano("Personal");
+        pizza1.setMasa("Normal");
+        pizza3.datos();
+        pizza2.datos();
+        pizza1.datos();
     }    
 }
