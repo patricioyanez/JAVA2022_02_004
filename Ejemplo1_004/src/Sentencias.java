dc0c7dd38658cff9d2b3af11c0ecda5127daa052
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-22 18:08:55 -0400

    update

---
+++ Ejemplo1_004/src/Sentencias.java
@@ -0,0 +1,27 @@
+public class Sentencias {
+    // psvm
+    public static void main(String[] args) {    
+        System.out.println("Sentencias iniciado...");
+        // definir variables
+        int numero = 20;
+        int numero2;
+        numero2 = 20;
+        // sentencia IF
+        if(numero == numero2)
+        {
+            System.out.println("Los nros son iguales");
+        }
+        numero2 = 40;
+        
+        if(numero == numero2)
+        {
+            System.out.println("Los nros son iguales.");
+        }
+        else
+        {
+            System.out.println("Los nros NO son iguales.");
+        }
+        // leer 211
+    }
+    
+}
