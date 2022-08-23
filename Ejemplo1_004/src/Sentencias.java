bb815cb72b78152af92674fa37893227b133ecaf
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-23 15:10:02 -0400

    update

--- Ejemplo1_004/src/Sentencias.java
+++ Ejemplo1_004/src/Sentencias.java
@@ -21,6 +21,28 @@ public class Sentencias {
         {
             System.out.println("Los nros NO son iguales.");
         }
+        
+        int edad = 10;
+        if(edad < 3)
+        {
+            System.out.println("Es un bebé");
+        }
+        else if(edad < 14)
+        {
+            System.out.println("Es un niño(a)");
+        }
+        else if(edad < 19)
+        {
+            System.out.println("Es un adolescente");
+        }        
+        else if(edad < 66)
+        {
+            System.out.println("Es un adulto");
+        }
+        else
+        {
+            System.out.println("Es adulto mayor");
+        }
         // leer 211
     }
     
