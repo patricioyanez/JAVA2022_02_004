5aa134885431e005e210096f65ea56a473321d40
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-11-07 16:35:34 -0300

    update

---
+++ Exp 5 y 6/InterfazGrafica_004/src/Modelo/Administrativo.java
@@ -0,0 +1,13 @@
+
+package Modelo;
+
+
+public class Administrativo {
+    private int rut;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private double altura;
+    private char genero;
+    private long sueldo;
+}
