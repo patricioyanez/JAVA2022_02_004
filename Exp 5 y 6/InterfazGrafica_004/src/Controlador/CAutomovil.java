78c66ad1b12a42612ab7d94a1c217819cd909b8e
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-11-08 14:40:59 -0300

    Update CAutomovil.java

--- Exp 5 y 6/InterfazGrafica_004/src/Controlador/CAutomovil.java
+++ Exp 5 y 6/InterfazGrafica_004/src/Controlador/CAutomovil.java
@@ -1,12 +1,15 @@
 package Controlador;
 import Conexion.Conexion;
 import Modelo.Automovil;
+import java.sql.Connection;
 
 public class CAutomovil {
     private Conexion conexion = new Conexion();
     public boolean agregar(Automovil automovil)
     {
         Connection cnx = conexion.obtenerConexion();
+        
+        String sql = "INSERT INTO AUTOMOVIL"
         return true;
     }
 }
